f7d74e6a479d094c08b9060d5ba2cfe094de99c1
undadbasicadj <unidadbasicadj.webmaster@gmail.com>
2025-01-06 13:25:14 -0300

    En la rama principal  Tu rama está actualizada con 'origin/principal'.
     Cambios a ser confirmados:
    	modificados:     javascript/inicio.js

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -64,14 +64,14 @@ window.onload = function () {
 
 // **** Función para recargar la página que evita el error del widget de Instagram en el host ****
 // Chequea si ya fue recargada
-/*
-if(!sessionStorage.getItem('reloaded') && !/Android/i.test(navigator.userAgent)) {
+
+if (!sessionStorage.getItem('reloaded') && !/Android/i.test(navigator.userAgent)) {
   // Esteblece el temporizador de la recarga
   setTimeout(function () {
     // Estblece el valor bandera en el almacenamiento de sesión
     sessionStorage.setItem('reloaded', 'true');
     // Recarga la págona
     location.reload();
-  }, 3000); // 3000 ms = 3 segundos
+  }, 2000); // 3000 ms = 3 segundos
 }
-*/
+
